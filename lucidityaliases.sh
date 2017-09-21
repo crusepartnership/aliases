5d4e7eec8a661aac7552af2e266a25a6418182ce
Tristan Kenney <tristan@kenney.co>
2017-09-21 18:50:28 +1000

    Added pbcopy to jira command

--- lucidityaliases.sh
+++ lucidityaliases.sh
@@ -1,5 +1,6 @@
 alias gitlc="git rev-parse --abbrev-ref HEAD | xargs git log --graph --abbrev-commit --decorate  --first-parent | grep -e '\[.*\]' -m 1 | cut -c 7-"
-alias jira="gitlc | sed -E 's/.*\[(.+)\].*/\1/g'"
-alias viewjira="jira | xargs -I{} -n1 bash -c 'open "https://integralcs.atlassian.net/browse/{}"'"
+alias _jira="gitlc | sed -E 's/.*\[(.+)\].*/\1/g'"
+alias jira="_jira | pbcopy && _jira"
+alias viewjira="_jira | xargs -I{} -n1 bash -c 'open "https://integralcs.atlassian.net/browse/{}"'"
 
 
