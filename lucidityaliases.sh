36dd95c07daa42fedeafffcc44ed45240ec55a3d
Tristan Kenney <tristan@kenney.co>
2017-09-22 11:01:37 +1000

    jira: trimmed newline before pbcopy

--- lucidityaliases.sh
+++ lucidityaliases.sh
@@ -1,6 +1,6 @@
 alias gitlc="git rev-parse --abbrev-ref HEAD | xargs git log --graph --abbrev-commit --decorate  --first-parent | grep -e '\[.*\]' -m 1 | cut -c 7-"
 alias _jira="gitlc | sed -E 's/.*\[(.+)\].*/\1/g'"
-alias jira="_jira | pbcopy && _jira"
-alias viewjira="_jira | xargs -I{} -n1 bash -c 'open "https://integralcs.atlassian.net/browse/{}"'"
+alias jira="_jira | tr -d '\n' | pbcopy && _jira"
+alias viewjira="jira | xargs -I{} -n1 bash -c 'open "https://integralcs.atlassian.net/browse/{}"'"
 
 
