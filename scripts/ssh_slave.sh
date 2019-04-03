3babb91283a0878b96f1aeb24de42e0a7f24965b
Tristan Kenney <tristan@kenney.co>
2019-04-03 15:15:03 +1100

    Updated with ascii art

--- scripts/ssh_slave.sh
+++ scripts/ssh_slave.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 SLAVE_IP=$(aws ec2 describe-instances --filters "Name=spot-instance-request-id,Values=$1" "Name=instance-state-name,Values=running" --query="Reservations[0].Instances[0].PublicIpAddress" | tr -d '"')
+if [[ $SLAVE_IP == 'null' ]]; then
+    SLAVE_IP=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$1" "Name=instance-state-name,Values=running" --query="Reservations[0].Instances[0].PublicIpAddress" | tr -d '"')
+fi
 SLAVE_DETAILS="ubuntu@"$SLAVE_IP
 
 ssh -i ~/jenkins-slaves.pem $SLAVE_DETAILS
