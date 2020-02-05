1355aaf8ec27bf9f77f15c770981a65d665cfab2
Chintogtokh Batbold <bchintogtokh@gmail.com>
2020-02-05 15:29:22 +1100

    Add proxy and sftp

--- scripts/proxy_slave.sh
+++ scripts/proxy_slave.sh
@@ -4,4 +4,5 @@ if [[ $SLAVE_IP == 'null' ]]; then
     SLAVE_IP=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$1" "Name=instance-state-name,Values=running" --query="Reservations[0].Instances[0].PublicIpAddress" | tr -d '"')
 fi
 SLAVE_DETAILS="ubuntu@"$SLAVE_IP
-sftp -i ~/jenkins-slaves.pem $SLAVE_DETAILS
+
+ssh -vvv -D 8080 -i ~/jenkins-slaves.pem $SLAVE_DETAILS
